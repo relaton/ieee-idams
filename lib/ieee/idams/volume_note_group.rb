b0ef16a2028614bcaeb35c6bbee6bf94bd4482e3
Ronald Tse <ronald.tse@ribose.com>
2024-11-13 13:07:15 +0800

    feat: add notegroup

---
+++ lib/ieee/idams/volume_note_group.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Ieee
+  module Idams
+
+    # Group of notes
+    class VolumeNoteGroup < Lutaml::Model::Serializable
+
+      # Textual note
+      # @return [String] A textual note
+      attribute :note, :string, collection: true
+
+      xml do
+        root "notegroup"
+        map_element "note", to: :note
+      end
+    end
+  end
+end
